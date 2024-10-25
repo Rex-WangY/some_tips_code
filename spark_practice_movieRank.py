caae6c4b930ffc9d7d065a3717918f450b59988f
Rex WangY <rex.wangy@doogroup.com>
2024-10-25 20:02:31 +0800

    enhance

--- spark_practice_movieRank.py
+++ spark_practice_movieRank.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-
+import pyspark.sql.functions as F
 if __name__ == "__main__":
     #0. create perfermance entry 
     spark = SparkSession.builder.\
@@ -21,5 +21,32 @@ if __name__ == "__main__":
         option("encoding", "utf-8").\
         schema(schema=schema).\
         load('../data/input/sql/u.data')
-    
-        
+
+    #TODO 1: user average rank
+    df.groupBy('user_id').\
+        avg('rank').\
+        withColumnRenamed("avg(rank)", "avg_rank").\
+        withColumn("avg_rank", F.round("avg_rank",2)).\
+        orderBy("avg_rank", ascending=False).\
+        show()
+
+    #TODO 2: movie average rank
+    df.groupBy('movie_id').\
+        avg('rank').\
+        withColumnRename("avg(rank)","avg_rank").\
+        withColumn("avg_rank", F.round("avg_rank",2)).\
+        orderBy('avg_rank',ascending = False).\
+        show()
+
+    #TODO 3 check the movie which movie rank biger than average rank
+    df.createTempView("movie")
+    spark.sql(
+        """
+           WITH 
+           avg_rank AS (
+            SELECT AVG(rank) as avg_rank FROM movie GROUP BY customer_id
+           )
+           SELECT movie_id FROM movie where rank > (select * from avg_rank)
+        """
+    )
+
